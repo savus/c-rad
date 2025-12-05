0701b615ff3005eaa94086eb8be265244a8d19af
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2025-12-05 09:38:39 -0700

    build out portfolio-cards

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
 const isVisible = "is-visible";
 
+const dataFilter = "[data-filter]";
+const dataItem = "[data-item]";
+const searchBox = document.getElementById("search");
+
 const root = document.documentElement;
 
 /* Theme */
@@ -19,6 +23,10 @@ const root = document.documentElement;
 const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
+
+/*Portfolio */
+const filterLinks = document.querySelectorAll(dataFilter);
+const portfolioCards = document.querySelectorAll(dataItem);
 
 /* Modal */
 const openModal = document.querySelectorAll(modalOpen);
@@ -58,6 +66,30 @@ for (const element of switcher) {
     const toggle = this.dataset.toggle;
     setActive(switcherBtn, this);
     setTheme(toggle);
+  });
+}
+
+searchBox.addEventListener("keyup", ({ target: { value } }) => {
+  const searchInput = value.toLowerCase().replace(/\s/g, "");
+  portfolioCards.forEach((card) => {
+    if (searchInput === "all") card.style.display = "block";
+    else if (card.dataset.item.includes(searchInput))
+      card.style.display = "block";
+    else card.style.display = "none";
+  });
+});
+
+for (const link of filterLinks) {
+  link.addEventListener("click", function ({ target }) {
+    const filter = this.dataset.filter;
+    console.log(filter);
+    setActive(dataFilter, target);
+
+    portfolioCards.forEach((card) => {
+      if (filter === "all") card.style.display = "block";
+      else if (filter === card.dataset.item) card.style.display = "block";
+      else card.style.display = "none";
+    });
   });
 }
 
