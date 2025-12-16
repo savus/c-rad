8461554c15733a9905c2654a61d2b2d803a0974e
savus <justin.fuzzygreenstuff@gmail.com>
2025-12-16 12:36:06 -0700

    Hook up functionality to carousel buttons so that the slides are rotated to the right or left by the corresponding arrow buttons

--- js/carousel.js
+++ js/carousel.js
@@ -5,9 +5,34 @@ let current = Math.floor(Math.random() * slides.length);
 let next = current < slides.length - 1 ? current + 1 : 0;
 let prev = current > 0 ? current - 1 : slides.length - 1;
 
-const dummySlides = [
-  //slide 0
-  //slide 1
-  //slide 2
-  //slide 3
-];
+const update = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active", "prev", "next");
+  });
+  slides[prev].classList.add("prev");
+  slides[current].classList.add("active");
+  slides[next].classList.add("next");
+};
+
+const goToNum = (num) => {
+  current = num;
+  next = current < slides.length - 1 ? current + 1 : 0;
+  prev = current > 0 ? current - 1 : slides.length - 1;
+  update();
+};
+
+console.log(`prev: ${prev}`, `current: ${current}`, `next: ${next}`);
+
+const goToPrev = () =>
+  current > 0 ? goToNum(current - 1) : goToNum(slides.length - 1);
+
+const goToNext = () =>
+  current < slides.length - 1 ? goToNum(current + 1) : goToNum(0);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () =>
+    i === 0 ? goToPrev() : goToNext()
+  );
+}
+
+update();
